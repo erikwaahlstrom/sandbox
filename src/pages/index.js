b7a09b59c8571de01a44806a9701ee0cfa6e6915
Erik Wahlström <erikwaahlstrom@gmail.com>
2019-03-13 11:03:52 +0100

    deleted seo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 
 import AppFrame from "../components/AppFrame"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Image from "../components/Image"
 
 const IndexPage = () => (
   <AppFrame>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <h1>First page</h1>
       <Image />
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,11 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import AppFrame from "../components/AppFrame"
-import SEO from "../components/seo"
 
 const SecondPage = () => (
   <AppFrame>
-    <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <Link to="/">Go back to the homepage</Link>
   </AppFrame>
