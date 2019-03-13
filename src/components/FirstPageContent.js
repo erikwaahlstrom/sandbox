d2fdf6e3d588829541028bb518ce88beb7e8580c
Erik Wahlström <erikwaahlstrom@gmail.com>
2019-03-13 14:48:36 +0100

    seperated assets into puffs

--- src/components/FirstPageContent.js
+++ src/components/FirstPageContent.js
@@ -8,16 +8,15 @@ import AppFrame from "../components/AppFrame"
 const SecondPage = () => (
   <AppFrame>
     <StaticQuery
-      query={secoundPageImgQuery}
+      query={firstPageImgQuery}
       render={data => {
         return (
           <>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-              <h1>Second Page</h1>
+              <h1>First page</h1>
               <Image fluid={data.avatar.childImageSharp.fluid} alt="zimply" />
-              <Image fluid={data.astro.childImageSharp.fluid} alt="zimply" />
             </div>
-            <Link to="/">Page 1</Link>
+            <Link to="/secound-page/">Page 2</Link>
           </>
         )
       }}
@@ -25,16 +24,9 @@ const SecondPage = () => (
   </AppFrame>
 )
 
-const secoundPageImgQuery = graphql`
+const firstPageImgQuery = graphql`
   query {
     avatar: file(relativePath: { eq: "zimply_stor.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    astro: file(relativePath: { eq: "gatsby-astronaut.png" }) {
       childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import AppFrame from "../components/AppFrame"
-import Image from "../components/Image"
+import FirstPageContent from "../components/FirstPageContent"
 
 const IndexPage = () => (
   <AppFrame>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <h1>First page</h1>
-      <Image />
-    </div>
-    <Link to="/secound-page/">Page 2</Link>
+    <FirstPageContent />
   </AppFrame>
 )
 
