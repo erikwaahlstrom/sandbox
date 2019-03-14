8cf8c4f81703694226e217bf71029dc3ef355268
Erik Wahlström <erikwaahlstrom@gmail.com>
2019-03-14 11:03:51 +0100

    added first page heading

--- src/components/FirstPageContent.js
+++ src/components/FirstPageContent.js
@@ -5,6 +5,7 @@ import GridImageComponent from "../components/GridImageComponent"
 
 const SecondPage = () => (
   <AppFrame>
+    <h1>First Page</h1>
     <GridImageComponent />
     <Link to="/second-page">Page 2</Link>
   </AppFrame>
